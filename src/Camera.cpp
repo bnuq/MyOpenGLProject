c9800ffac9a0f3823575a74808d9e0e241435e01
bnuq <2astwis2@gmail.com>
2022-08-01 14:55:21 +0900

    camera done

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,11 +1,13 @@
 #include "Camera.h"
 
+
 CameraPtr Camera::Create(CharacterPtr player)
 {
     auto camera = CameraPtr(new Camera());
     camera->Init(player);
     return camera;
 }
+
 
 
 // 카메라 초기화
@@ -21,10 +23,11 @@ void Camera::Init(CharacterPtr player)
     FrontVec = glm::normalize(mainChar->FrontVec);
     LeftVec  = glm::normalize(mainChar->LeftVec);
     UpVec    = glm::normalize(mainChar->UpVec);
+
     // 카메라 위치 초기화
     Position = CameraPivot + (-FrontVec) * xzDist + UpVec * yDist;
 
-    // 카메라가 바라보는 방향
+    // 카메라가 바라보는 방향 = 캐릭터를 바라보는 방향
     Direction = glm::normalize(CameraPivot - Position);
 }
 
@@ -33,17 +36,17 @@ void Camera::Init(CharacterPtr player)
 // 카메라의 위치를 계산하는 함수 => 피벗의 위치를 메인 캐릭터에 맞춰서 이동
 void Camera::SetPosition()
 {
-    // 거리 세팅이 변하면, 반영
-    Position = CameraPivot + (-FrontVec) * xzDist + UpVec * yDist;
-
     // 메인 캐릭터와 피벗 사이의 벡터
     auto PivotToChar = mainChar->Position - CameraPivot;
     
-    // 해당 벡터 길이가 일정 이하면 움직이지 않는다
-    if(!CameraMove && glm::length(PivotToChar) < CamToCharMaxLimit) return;
+    // 카메라가 움직이지 않는 상태였고 &&
+    // 캐릭터 ~ 피벗 사이의 길이가 일정 이하면 움직이지 않는다
+    if(!CameraMove && glm::length(PivotToChar) < CamToCharMaxLimit)
+        return;
     
 
-    // 움직이기로 결정했으면
+
+    // 움직이기로 결정했으면 ~ 캐릭터와 피벗 사이의 거리가 일정 이하가 될 때까지 카메라가 움직니다
     CameraMove = true;
 
     // 카메라 속도로 메인 카메라를 따라간다
@@ -57,6 +60,7 @@ void Camera::SetPosition()
 }
 
 
+
 /* 
     glm::rotate 
         => 임의의 축을 중심으로 회전하는 쿼터니언을 생성하는 건 맞는데
@@ -64,11 +68,15 @@ void Camera::SetPosition()
         원점을 지나는 축에 대한 회전을 나타낸다
 
         따라서 이번 경우처럼 특정 pivot 을 중심으로 회전을 하는 경우
-        원점에서의 회전으로 변경한 다음, 회전하고 다시 원상복구하는 과정이 필요하다
+            피벗 -> 원점 으로 이동
+            원점에서 회전하고 
+            다시 원점 -> 피벗 으로 원상복구
+        하는 과정이 필요하다
  */
-// 카메라를 회전 시키는 역할 => 피벗 기준으로 회전한다
+// 카메라를 회전 시키는 함수 => 피벗 기준으로 회전한다
 void Camera::Rotate(glm::vec2 deltaPos)
 {
+    // Yaw => 카메라의 Up Vector 기준 회전 => 무조건 (0, 1, 0) 기준 회전
     auto yawAngle = (-deltaPos.x) * yawRotSpeed;
         if(yawAngle < 0)    yawAngle += 360;
         if(yawAngle > 360)  yawAngle -= 360;
@@ -78,16 +86,22 @@ void Camera::Rotate(glm::vec2 deltaPos)
     auto comeBack = glm::translate(glm::mat4(1.0f), CameraPivot);
 
     Position = glm::vec3((comeBack * yawRotate * goToOrigin) * glm::vec4(Position, 1.0f));
+
     // 카메라 좌표계도 회전시켜 주어야 하는데, 그냥 회전 변환 적용하면 된다
     FrontVec = glm::vec3(yawRotate * glm::vec4(FrontVec, 0.0f));
     LeftVec = glm::vec3(yawRotate * glm::vec4(LeftVec, 0.0f));
     Direction = glm::vec3(yawRotate * glm::vec4(Direction, 0.0f));
 
-    // pitchAngle 은 제한이 있다 + 값을 누적한다
+
+    /* ****** */
+
+
+    // Pitch => 카메라의 Left Vector 기준 회전 => 오른손 좌표계 사용
+    // pitchAngle 은 제한이 있다 + 값을 누적한다 => 왜? 항상 정면을 바라본 상태에서 어느정도 각도로 회전하느냐를 따지니까
     auto pitchAngle = (deltaPos.y) * pitchRotSpeed;
         pitchAngle = glm::clamp(pitchAngle, pitchRotLimit.x, pitchRotLimit.y);
 
-    // Direction 세팅
+    // 좌표계를 회전하지 않고, Direction 벡터를 회전시킨다
     auto pitchRotate = glm::rotate(glm::mat4(1.0f), glm::radians(pitchAngle), LeftVec); // 바뀐 LeftVec 에 대해서
     Direction = glm::vec3(pitchRotate * glm::vec4(Direction, 0.0f));
 }
--- src/Camera.h
+++ src/Camera.h
@@ -5,50 +5,55 @@
 #include "Character.h"
 
 
+// 카메라의 정보를 가지고 있는 카메라 객체
 CLASS_PTR(Camera)
 class Camera
 {
 public:
-    // 카메라가 바라보는 점이자 회전의 중심 피벗
+    // 피벗 = 카메라가 바라보는 점이자 회전의 중심
     glm::vec3 CameraPivot;
-        float CamToCharMaxLimit = 0.1f;
-        float CamToCharMinLimit = 0.01f;
+        float CamToCharMaxLimit = 0.1f;                         // 카메라 피벗이 움직이게 하는 거리
+        float CamToCharMinLimit = 0.01f;                        // 카메라 피벗이 더이상 움직이지 않게 하는 거리
+        bool CameraMove = false;                                // 카메라가 움직이고 있는 지 ~ 상태
 
-    bool CameraMove = false;
-        float CameraMoveSpeed = 0.01f;
+    
+    float CameraMoveSpeed = 0.01f;
 
-        float yawRotSpeed { 0.1f };
-        float pitchRotSpeed { 0.1f };
+    float yawRotSpeed { 0.1f };
+    float pitchRotSpeed { 0.1f };
         glm::vec2 pitchRotLimit { glm::vec2{-10.0f, 30.0f} };
 
 
-    // 카메라의 위치 초기화
+
+    // 카메라의 위치
     glm::vec3 Position;
 
     // 카메라 모델 좌표계
     glm::vec3 FrontVec          = glm::vec3(0.0f, 0.0f, 1.0f);
     glm::vec3 LeftVec           = glm::vec3(1.0f, 0.0f, 0.0f);
     glm::vec3 UpVec             = glm::vec3(0.0f, 1.0f, 0.0f);
+
     // 카메라가 바라보는 방향
     glm::vec3 Direction         = glm::vec3(0.0f, 0.0f, 1.0f);
     
 
-/* 카메라 위치, 거리 */
-    float xzDist = 40.0f;
-    float yDist = 30.0f;
 
-    
-    // 카메라가 따라다니는 플레이어
+    // 카메라가 따라다니는 캐릭터
     CharacterPtr mainChar;
+        // 카메라가 캐릭터로 부터 떨어진 거리 
+        float xzDist = 40.0f;
+        float yDist = 30.0f;
 
 
-
+    // 생성 및 초기화
     static CameraPtr Create(CharacterPtr player);
     void Init(CharacterPtr player);
     
 
-    // 카메라를 따라간다 + 땅바닥 높이를 바라본다
+    // 움직인 캐릭터에 맞춰, 카메라를 이동시킴
     void SetPosition();
+
+
     // 마우스를 움직이는 만큼, 카메라가 회전한다 + 바라보는 Direction 벡터도 달라진다
     void Rotate(glm::vec2 deltaPos);
 
